946258f2e530ed849744c33873bb04bb2cea4ca9
Alex Davis <alex2006hw@gmail.com>
2015-04-20 22:47:02 -0700

    Update t.js

--- backend/t.js
+++ backend/t.js
@@ -2,7 +2,7 @@
 
 var s = require(__dirname + "/sms.js");
 
-var phoneNumber = "14155988551";
+var phoneNumber = "1-xxx-xxx-xxxx";
 var smsMessage = "Welcome to closer.pointlook.com";
 
 s.sendMessage(phoneNumber, smsMessage);
